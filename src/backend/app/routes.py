b79ec7c872fdecef551ed578745c81d102559588
GregT7 <gregterrelga@gmail.com>
2025-09-04 20:21:52 -0400

    made http response objects more detailed, also added error handling + messaging for /api/health and /api/stats

--- src/backend/app/routes.py
+++ src/backend/app/routes.py
@@ -1,8 +1,9 @@
-import flask, json
+import flask, json, time
 from flask import jsonify, request
 from datetime import datetime
 from app import app, supabase
 from utils import *
+
 
 
 @app.route('/')
@@ -11,40 +12,38 @@ def index():
 
 @app.route('/api/health', methods=['GET'])
 def determine_health():
-    return jsonify({'ok': True, 'now': datetime.now(), 'version': flask.__version__})
-
-@app.route('/api/get_db_data', methods=['GET'])
-def get_data():
+    start_time = time.perf_counter()
     try:
-        response = supabase.table("work").select("*").execute()
-        return jsonify(response.data), 200
+        http_response = {
+            "ok": True,
+            "service": "flask",
+            "version": flask.__version__,
+            "now": datetime.now(),
+            "response_time_ms": (time.perf_counter() - start_time) * 1000
+        }
+        return jsonify(http_response)
     except Exception as e:
-        print("Error:", str(e))
+        http_response = {
+            "ok": False,
+            "error": str(e),
+            "now": datetime.now(),
+            "response_time_ms": (time.perf_counter() - start_time) * 1000
+        }
+        return jsonify(http_response)
 
 @app.route('/api/stats', methods=['GET'])
 def calc_stats():
+    start_time = time.perf_counter()
     try:
-
-        # curl "http://127.0.0.1:5000/api/stats?start=2025-07-01&end=2025-07-31"
         # curl "http://127.0.0.1:5000/api/stats?start=2025-06-01&end=2025-06-30"
-        # curl "http://127.0.0.1:5000/api/stats?start=2025-04-01&end=2025-04-30"
-        # curl "http://127.0.0.1:5000/api/stats?start=2025-02-13&end=2025-03-23"
-
-
         start_arg = request.args.get('start') # ?start=2024-06-14
         end_arg = request.args.get('end') # ?end=2025-08-24
-
-
 
         fmt = "%Y-%m-%d"
         start = datetime.strptime(start_arg, fmt)
         end = datetime.strptime(end_arg, fmt)
 
         date_range = find_valid_range(start, end)
-
-
-
- 
 
         query = supabase.table("work").select("*") \
         .gte('completion_date', date_range['start']) \
@@ -58,7 +57,30 @@ def calc_stats():
             'day': calc_day_stats(date_range, data)
         }
 
-        return jsonify(statistic_data)
+
+        end_time = time.perf_counter()
+        elapsed_time = (end_time - start_time) * 1000
+
+        http_response = {
+            "ok": True,
+            "num_records": len(data),
+            "db_connected": True,
+            "params": {'start_date': start_arg, 'end_date': end_arg},
+            "response_time_ms": elapsed_time,
+            "now": datetime.now(),
+            "data": statistic_data
+        }
+
+        return jsonify(http_response)
         
     except Exception as e:
-        print("Error:", str(e))
+        end_time = time.perf_counter()
+        elapsed_time = (end_time - start_time) * 1000
+        http_response = {
+            "ok": False,
+            "error": str(e),
+            "response_time_ms": elapsed_time,
+            "now": datetime.now()
+        }
+
+        return jsonify(http_response)
